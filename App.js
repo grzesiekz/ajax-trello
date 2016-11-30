d5c66f80d326889a8102a1442b549d177c782acb
Grzegorz Żuchowicz <grzegorz@zuchowicz.net>
2016-11-30 20:27:38 +0100

    Trello-a-like with Ajax. Initial commit

---
+++ App.js
@@ -0,0 +1,33 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	'X-Client-Id': '127',
+	'X-Auth-Token': 'dc326b360f8a7fb447e97be987b8373d'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+	url: baseUrl + '/board',
+	method: 'GET',
+	success: function(response) {
+		setupColumns(response.columns);
+	}
+});
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+		var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
+}
+
